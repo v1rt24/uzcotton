3bd06c3d6744eea54b681bb81f3304f2fc9a7bff
v1rt24 <v1rt24@mail.ru>
2025-10-29 03:13:39 +0300

    Готова шапка в ПК версии

---
+++ main.ts
@@ -0,0 +1,8 @@
+// Подключение общего файла css
+import './assets/css/index.css';
+
+// === @license Главная страница ===
+import './index/js/index';
+
+// === @license Другая страница ===
+import './nested/js/index';
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  build: {
+    minify: false,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'nested/index.html'),
+      },
+    },
+  },
+});
